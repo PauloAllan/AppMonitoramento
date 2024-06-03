00d58e07fc178f46b2edd5e7e4b3d4f8e2fb2e3e
Paulo Allan <pauloosilbat@gmail.com>
2024-06-03 15:22:45 -0300

    Adicionando método reset password

--- src/app/configuracao.ts
+++ src/app/configuracao.ts
@@ -1,9 +1,3 @@
 export const configuracao = {
-    apiKey: "AIzaSyDP9atXKnAhXv1gyCC_R-Xu7Dpn3olZLRA",
-    authDomain: "appmonitoramento-424616.firebaseapp.com",
-    projectId: "appmonitoramento-424616",
-    storageBucket: "appmonitoramento-424616.appspot.com",
-    messagingSenderId: "1008438447377",
-    appId: "1:1008438447377:web:15421885a30557d5e350b0",
-    measurementId: "G-RXGJF557GD"
+    
   };
